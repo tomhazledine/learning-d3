07be7efc6f8a3eb0356729690380020b72f48392
Tom Hazledine <tom@eatenbymonsters.com>
2015-10-11 16:51:34 +0100

    basic style manipulation

--- uncompressed/js/custom/main.js
+++ uncompressed/js/custom/main.js
@@ -1,1 +1,50 @@
 console.log('scripts are still working');
+
+var myStyles = [
+    {
+        colour: '#a57706',
+        width: 4
+    },
+    {
+        colour: '#bd3613',
+        width: 4.2
+    },
+    {
+        colour: '#d11c24',
+        width: 4.8
+    },
+    {
+        colour: '#c61c6f',
+        width: 3.8
+    },
+    {
+        colour: '#595ab7',
+        width: 4.6
+    },
+    {
+        colour: '#ab7595',
+        width: 2
+    },
+    {
+        colour: '#b75a59',
+        width: 5
+    },
+    {
+        colour: '#268bd2',
+        width: 4.2
+    }
+];
+
+var testItem = d3.selectAll('.item');
+
+testItem
+    .data(myStyles)
+    .style({
+        'color': 'white',
+        'background' : function(d){
+            return d.colour;
+        },
+        'width' : function(d){
+            return d.width + 'em';
+        }
+    })
