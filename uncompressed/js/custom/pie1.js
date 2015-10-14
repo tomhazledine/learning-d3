796a17b5953946e8d65f368b7ee1941fdf60e825
Tom Hazledine <tom@tomhazledine.com>
2015-10-14 08:46:09 +0100

    added labels to pie

--- uncompressed/js/custom/pie1.js
+++ uncompressed/js/custom/pie1.js
@@ -3,8 +3,21 @@ function drawPieChart(data){
 	var // Sizes
 		width = 400,
 		height = 400,
-		radius = 200,
-		colours = d3.scale.category20c();
+		radius = 200;
+
+	var // Colours
+		coloursPreset = d3.scale.category20c(),
+		colours = d3.scale.ordinal()
+			.range([
+				'#595ab7',
+				'#a57706',
+				'#d11c24',
+				'#c61c6f',
+				'#bd3613',
+				'#2176c7',
+				'#259286',
+				'#738a05'
+			]);
 
 	var pie = d3.layout.pie()
 		.value(function(d){
@@ -13,7 +26,7 @@ function drawPieChart(data){
 
 	var arc = d3.svg.arc()
 		.outerRadius(radius)
-		.innerRadius(radius * .6)
+		.innerRadius(radius * .5)
 
 	var myPieChart = d3.select('#chartFour').append('svg')
 		.attr('width',width)
@@ -21,9 +34,27 @@ function drawPieChart(data){
 		.append('g')
 			.attr('transform','translate(' + (width - radius) + ', ' + (height - radius) + ')')
 			.selectAll('path').data(pie(data))
-			.enter().append('path')
-				.attr('fill',function(d,i){
-					return colours(i);
-				})
-				.attr('d', arc)
+			.enter().append('g')
+				.classed('slice', true)
+
+	var slices = d3.selectAll('g.slice')
+		.append('path')
+		.attr('fill',function(d,i){
+			return colours(i);
+		})
+		.attr('d', arc)
+
+	var text = d3.selectAll('g.slice')
+		.append('text')
+		.text(function(d,i){
+			// console.log(d);
+			return d.data.label;
+		})
+		.attr('text-anchor','middle')
+		.attr('fill', 'white')
+		.attr('transform',function(d){
+			d.innerRadius = 0;
+			d.outerRadius = radius;
+			return 'translate(' + arc.centroid(d) + ')';
+		})
 }
