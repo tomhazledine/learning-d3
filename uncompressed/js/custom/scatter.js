685ed6325fbe67865f8e0e4b6878842696e0c0da
Tom Hazledine <tom@tomhazledine.com>
2015-10-21 08:59:49 +0100

    basic scatter

--- uncompressed/js/custom/scatter.js
+++ uncompressed/js/custom/scatter.js
@@ -1,3 +1,42 @@
+var wrapper = d3.select('#basicScatterGraph');
+
+var margin = {
+    top: 10,
+    right: 10,
+    bottom: 10,
+    left: 10
+};
+
+var wrapperWidth = parseInt(wrapper.style('width')),
+	wrapperHeight = parseInt(wrapper.style('height')),
+	height = wrapperHeight - margin.top - margin.bottom,
+	width = wrapperWidth - margin.left - margin.right
+
+//---
+
+var scatterSvg = wrapper.append('svg')
+    .attr('width',wrapperWidth)
+    .attr('height',wrapperHeight);
+
+var xScale = d3.scale.linear().range([0, wrapperWidth]);
+var yScale = d3.scale.linear().range([wrapperHeight, 0]);
+
+function renderScatter(data){
+	xScale.domain(d3.extent(data, function (d){ return d.sepal_length; }));
+	yScale.domain(d3.extent(data, function (d){ return d.petal_length; }));
+
+	var circles = scatterSvg.selectAll('circle').data(data);
+	circles.enter().append('circle').attr('r',10);
+
+	circles
+		.attr('cx',function (d){ return xScale(d.sepal_length); })
+		.attr('cy',function (d){ return yScale(d.petal_length); });
+
+	circle.exit().remove();
+}
+
+//---
+
 function type(d){
     d.sepal_length = +d.sepal_length;
     d.sepal_width = +d.sepal_width;
@@ -6,8 +45,4 @@ function type(d){
     return d;
 }
 
-d3.csv("/iris.csv", type, function(data){
-    var min = d3.min(data, function (d){ return d.sepal_length; });
-    var max = d3.max(data, function (d){ return d.sepal_length; });
-    console.log([min,max]);
-});
+d3.csv("/iris.csv", type, renderScatter);
