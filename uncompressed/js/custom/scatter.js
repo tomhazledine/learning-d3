ab71c35d752dec24b86ffad2f70a0ddc4a8aa801
Tom Hazledine <tom@tomhazledine.com>
2015-10-21 09:08:48 +0100

    scatter circle radius

--- uncompressed/js/custom/scatter.js
+++ uncompressed/js/custom/scatter.js
@@ -20,13 +20,14 @@ var scatterSvg = wrapper.append('svg')
 
 var xScale = d3.scale.linear().range([0, wrapperWidth]);
 var yScale = d3.scale.linear().range([wrapperHeight, 0]);
+var rScale = d3.scale.linear().range([0, 3]);
 
 function renderScatter(data){
 	xScale.domain(d3.extent(data, function (d){ return d.sepal_length; }));
 	yScale.domain(d3.extent(data, function (d){ return d.petal_length; }));
 
 	var circles = scatterSvg.selectAll('circle').data(data);
-	circles.enter().append('circle').attr('r',10);
+	circles.enter().append('circle').attr('r',function (d){ return rScale(d.sepal_width); });
 
 	circles
 		.attr('cx',function (d){ return xScale(d.sepal_length); })
